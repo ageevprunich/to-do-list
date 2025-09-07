e087fb4e81599f0a40822a447d3aefa25dc10c78
Serhii <ageev.prunich@gmail.com>
2025-09-07 22:24:55 +0300

    Start & header

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import './Header.scss';
+import React from 'react';
+import { ReactComponent as SunIcon } from '../assets/icon-sun.svg';
+
+
+
+function Header() {
+    return (
+        <div className="container">
+            <header className="header">
+                <div className="header__logo">
+                    <span>ToDo</span>
+                </div>
+                <div className="header__btn">
+                    <button className='theme-btn' id='theme-btn'>
+                        <SunIcon className='sun-icon'/>
+                    </button>
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
+      
---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,13 @@
+import './ToDoList.scss';
+import React from 'react';
+
+function ToDoList() {
+    return (
+
+        <div className="container">
+            <p>ToDo List Component</p>
+        </div>
+    );
+}
+
+export default ToDoList;
